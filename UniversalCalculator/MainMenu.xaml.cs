7945076625f4ae02f167a61500628b827e8af563
AmyWatson3 <Amy.WATSON3@student.tafesa.edu.au>
2026-04-01 14:21:22 +1030

    Main menu updated with logo image and buttons to access calculators Note: the button click events have been added but not implemented.

---
+++ UniversalCalculator/MainMenu.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Calculator
+{
+	public sealed partial class MainMenu : Page
+	{
+		// default var
+		public static MainMenu mainMenu { get; set; }
+
+		// default functions
+		public MainMenu()
+		{
+			InitializeComponent();
+
+			mainMenu = this;
+
+		}
+
+		// page functions
+		private void pageLoaded(object sender, RoutedEventArgs e)
+		{
+
+			// window minimum size
+			ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(256, 650));
+
+			// enable title bar full customiztion
+			CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+			// title bar customization
+			ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+			titleBar.ButtonBackgroundColor = Windows.UI.Colors.Transparent;
+			titleBar.ButtonInactiveBackgroundColor = Windows.UI.Colors.Transparent;
+			titleBar.ButtonInactiveForegroundColor = Windows.UI.Colors.White;
+		}
+
+		private void mathButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void mortgageButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void currencyButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void exitButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+	}
+}
